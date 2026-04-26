42a341df62d852eab12d0e585f93946ebb06dcce
HaewonJeong <bonjour_haewon@naver.com>
2026-04-26 22:50:02 +0900

    feat: 음악 목록 조회 기능 및 Zustand 스토어 구현

---
+++ src/app/store/useMusicStore.js
@@ -0,0 +1,19 @@
+import { create } from "zustand";
+import { musicApi } from "../api/MusicApi";
+
+export const useMusicStore = create( (set)=> ({
+    // 1. 상태 (State): 기존 useState([]) 역할
+    musics: [],
+
+    // 2. 액션 (Action): 기존 useEffect 안에 있던 데이터 가져오기 로직
+    async fetchMusics(){
+        try{
+            const data = await musicApi.getMusics();
+            set({musics:data}); 
+        }catch(error){
+            console.log("스토어에서 데이터 가져오기 실패",error);
+            set({musics:[]});
+        }
+    }
+
+}))
