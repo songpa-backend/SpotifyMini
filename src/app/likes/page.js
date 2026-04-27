b983bacefbd9b992cd653b1e5b0870ad7b4ee83f
namchaerin <cr434972@gmail.com>
2026-04-27 23:00:03 +0900

    좋아요 전체 해제 기능 추가 및 css 적용

--- src/app/likes/page.js
+++ src/app/likes/page.js
@@ -2,9 +2,10 @@
 import { useLikeStore } from "@/store/useLikeStore";
 import Link from "next/link";
 import { useEffect } from "react";
+import styles from "./likes.module.css";
 
 export default function LikesPage() {
-  const { favorites, fetchFavorites, toggleLike, isLoading } = useLikeStore();
+  const { favorites, fetchFavorites, toggleLike, clearAllLikes, isLoading } = useLikeStore();
 
   const userId = 1;
 
@@ -20,41 +21,51 @@ export default function LikesPage() {
 
   return (
     <div>
-      <h1>❤️ 좋아요한 곡 목록 </h1>
-      <Link href="/">
-        &larr; 홈
-      </Link>
-      {favorites.length === 0 ? (
-        <p>좋아요를 누른 노래가 없어요!</p>
-      ) : (
-        <ul>
+      <Link className={styles.homeButton} href="/">[홈]</Link>
+      <h2 style={{ textAlign: 'center', marginBottom: '15px' }}>❤️ 좋아요한 곡 목록 </h2>
+      <div className={styles.listSection}>
+        {favorites.length === 0 ? (
+          <p>좋아요를 누른 노래가 없어요!</p>
+        ) : (
+          <ul className={styles.musicList}>
             {favorites.map((fav) => {
-                const liked = true;
-
-                if (!fav.music) return null;
-
-                return (
-                <li key={fav.id}>
+              const liked = true;
+              
+              if (!fav.music) return null;
+              
+              return (
+                <li key={fav.id} className={styles.listItem}>
+                  <div className={styles.itemTitle}>
                     <button 
-                        onClick={() => toggleLike(fav.userId, fav.music.id)}
-                        style={{
-                        background: 'none',
-                        border: 'none',
-                        padding: 0,
-                        cursor: 'pointer'
-                        }}>
-                    [{liked ? "♥" : "♡"}]
-                    </button>
+                      className={styles.likeButton}
+                      onClick={() => toggleLike(fav.userId, fav.music.id)}
+                    >
+                      [{liked ? "♥" : "♡"}]
+                      </button>
+                      {fav.music.title} - {fav.music.artist}
+                      <Link href={`/music/${fav.music.id}`}>(상세보기 &gt;)</Link>
+                    </div>
+                  </li>
+                  );
+                  })}
+          </ul>
+        )}
 
-                    <b>
-                        {fav.music.title} - {fav.music.artist}
-                        <Link href={`/music/${fav.music.id}`}>(상세보기 &gt;)</Link>
-                    </b>
-                </li>
-                );
-                })}
-        </ul>
-      )}
+        <div className={styles.buttonSection}>
+            <button 
+              className={styles.clearButton}
+              onClick={() => {
+                if(confirm("정말로 모든 좋아요를 해제하시겠습니까?")) {
+                  clearAllLikes();
+                }
+              }}
+            >
+              [좋아요 전체 해제]
+            </button>
+          </div>
+        </div>
     </div>
+
+    
   );
 }
