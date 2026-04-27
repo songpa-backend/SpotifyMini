660d14281b8e93bb74f907c9940d5d50eb12cdb0
HaewonJeong <godnjs3211@gmail.com>
2026-04-27 14:37:38 +0900

    chore: resolve merge conflict

--- src/app/music/page.js
+++ src/app/music/page.js
@@ -7,6 +7,7 @@ import { useEffect, useState } from "react";
 export default function Music() {
 
     const { musics, fetchMusics } = useMusicStore();
+    const [searchValue, setSearchValue] = useState('');
     const { fetchFavorites, toggleLike, isLoading: likeLoading } = useLikeStore()
     const userId = 1; // 임시 데이터
 
@@ -15,37 +16,65 @@ export default function Music() {
     useEffect(() => {
       fetchMusics();
       fetchFavorites(userId);
-    }, []);
+    }, [fetchMusics]);
 
-    const [serachValue, setSearchValue] = useState('');    
-    const onClickHandler = () => {
-        console.log(`검색: ${serachValue}`);
-        //검색 기능 구현중...
+    const [searchedMusics, setSearchedMusics] = useState([]);
+    const [isSearched, setIsSearched] = useState(false); //검색 버튼 클릭 여부
+
+  const onClickHandler = () => {
+    console.log(`검색: ${searchValue}`);
+    if (!searchValue.trim()) { //검색어가 공백이 아닐때
+      setIsSearched(false); //검색 한적 없음
+      setSearchedMusics([]); //검색결과 빈배열
+      return;
     }
+    //검색 결과와 맞는 결과 searchValue에 추가       
+    const results = musics.filter(music => (music.title.toLowerCase().includes(searchValue.toLowerCase())) ||
+      (music.artist.toLowerCase().includes(searchValue.toLowerCase())));
+    setSearchedMusics(results);
+    setIsSearched(true); //검색 한적 있음으로 바꿈
+    console.log(searchedMusics);
+  }
 
     if (likeLoading) {
       return <div>데이터 로딩 중... 잠시만 기다려 주세요!</div>;
     } 
 
   return (
-    <> 
+    <>
       <div><Link href={""}>[전체곡]</Link> <Link href={""}>[&gt;&gt;&gt; 내 좋아요 목록]</Link></div>
+
       <div>[검색창:
-            <input type="search"
-                   name="musicName"
-                   value={serachValue}
-                   onChange={ (e)=> setSearchValue(e.target.value) }
-                   placeholder="곡명 또는 가수명"     
-            />]
-            <button onClick={onClickHandler} >검색</button>
+        <input type="search"
+          name="musicName"
+          value={searchValue}
+          onChange={(e) => {
+            setSearchValue(e.target.value);
+            if (e.target.value === "") { setIsSearched(false); } //검색창이 완전히 비었을 때만 전체 목록을 보여줌
+          }}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              onClickHandler();
+            }
+          }}
+          placeholder="곡명 또는 가수명"
+
+        />]
+        <button onClick={onClickHandler} >검색</button>
       </div>
+
       <div>곡 목록</div>
-      <ul>
-         {musics?.map(music => {
+      <ul>{
+         (isSearched && searchValue && searchedMusics.length === 0) ?
+         //검색 결과가 없을 때 (검색어 있고, 검색 결과가 없음)
+         <li style={{ color: 'gray' }} > 찾으시는 곡이 없어요.🥺 다른 단어로 검색해보세요. </li>
+         :
+         //보여줄 목록이 하나라도 있으면 표시
+        (searchedMusics.length > 0 ? searchedMusics : musics)?.map(music => {
           const liked = favorites.some(f => String(f.musicId) === String(music.id));
           return (
-            <li key={music.id}>
-              <button 
+          <li key={music.id}>
+                <button 
                 onClick={() => toggleLike(userId, music.id)}
                 style={{
                   background: 'none',
@@ -55,14 +84,13 @@ export default function Music() {
                 }}>
                 [{liked ? "♥" : "♡"}]
               </button>
-              <b>{music.title} - {music.artist}
-                <Link href={""}>(상세보기 &gt;)</Link>
-              </b>
-            </li>
-          )})}
+                <b>{music.title} - {music.artist}
+                  <Link href={""}>(상세보기 &gt;)</Link></b>
+              </li>
+            )})}
       </ul>
 
-      <div style={{color:'white', background: 'black', textAlign: 'center'}} >♡ = 좋아요 안함 / ♥ = 좋아요</div>
+      <div style={{ color: 'white', background: 'black', textAlign: 'center' }} >♡ = 좋아요 안함 / ♥ = 좋아요</div>
 
     </>
   );
