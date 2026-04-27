9d714bc32b38991df8487e6671c27f96094338b3
namchaerin <cr434972@gmail.com>
2026-04-27 15:54:43 +0900

    feat: 좋아요 목록 페이지 추가

---
+++ src/app/likes/page.js
@@ -0,0 +1,60 @@
+'use client'
+import { useLikeStore } from "@/store/useLikeStore";
+import Link from "next/link";
+import { useEffect } from "react";
+
+export default function LikesPage() {
+  const { favorites, fetchFavorites, toggleLike, isLoading } = useLikeStore();
+
+  const userId = 1;
+
+  useEffect(() => {
+    if (userId) {
+      fetchFavorites(userId);
+    }
+  }, [fetchFavorites, userId]);
+
+  if (isLoading && favorites.length === 0) {
+    return <div>좋아요 목록을 불러오는 중입니다... 🎧</div>;
+  }
+
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">❤️ 좋아요한 곡 목록 </h1>
+      <Link href="/" className="text-blue-500 underline mb-4 block">
+        &larr; 홈
+      </Link>
+      {favorites.length === 0 ? (
+        <p>좋아요를 누른 노래가 없어요!</p>
+      ) : (
+        <ul>
+            {favorites.map((fav) => {
+                const liked = true;
+
+                if (!fav.music) return null;
+
+                return (
+                <li key={fav.id} className="border-b py-2 flex items-center justify-between">
+                    <button 
+                        onClick={() => toggleLike(fav.userId, fav.music.id)}
+                        style={{
+                        background: 'none',
+                        border: 'none',
+                        padding: 0,
+                        cursor: 'pointer'
+                        }}>
+                    [{liked ? "♥" : "♡"}]
+                    </button>
+
+                    <b>
+                        {fav.music.title} - {fav.music.artist}
+                        <Link href={""}>(상세보기 &gt;)</Link>
+                    </b>
+                </li>
+                );
+                })}
+        </ul>
+      )}
+    </div>
+  );
+}
